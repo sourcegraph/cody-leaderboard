053bec837850b0f2b37ffb88635278f39f61527a
jamesmcnamara <james.mcnamara@sourcegraph.com>
2024-06-27 13:08:16 -0700

    opening PR with current unit tests

--- leaderboard.py
+++ leaderboard.py
@@ -68,17 +68,20 @@ def chat_csv_table(name: str):
     score_df = score_df.set_index("FIXTURE")
     st.dataframe(score_df)
 
-    pivot_df = df.pivot(index=["CHAT_QUESTION", "QUESTION_CLASS"], columns="FIXTURE", values="LLM_JUDGE_SCORE")
+    pivot_df = df.pivot(index=["CHAT_QUESTION", "QUESTION_CLASS"],
+                        columns="FIXTURE", values="LLM_JUDGE_SCORE")
     pivot_df = pivot_df.sort_values(by="QUESTION_CLASS")
     pivot_df = pivot_df[models_by_score]
 
     st.dataframe(pivot_df.style.map(
         lambda score: 'background-color: #ffdddd' if score == 0 else '', subset=pivot_df.columns))
 
-    selected_question = st.selectbox("Select question", df["CHAT_QUESTION"].unique())
+    selected_question = st.selectbox(
+        "Select question", df["CHAT_QUESTION"].unique())
     filtered_models = st.multiselect("Filter models", df["FIXTURE"].unique())
 
-    rows = df[(df["CHAT_QUESTION"] == selected_question) & ((len(filtered_models) == 0) | (df["FIXTURE"].isin(filtered_models)))]
+    rows = df[(df["CHAT_QUESTION"] == selected_question) & (
+        (len(filtered_models) == 0) | (df["FIXTURE"].isin(filtered_models)))]
     fixtures = rows.groupby("FIXTURE")
 
     for fixture, rows in fixtures:
@@ -110,12 +113,6 @@ def unit_test_csv_table(name: str):
     df = pd.read_csv(name)
     st.dataframe(df[['TEST_NAME', 'TEST_LANGUAGE']])
 
-    # filtered_df = df[["FIXTURE", "FILEPATH", "TEST_NAME",
-    #                   "LLM_JUDGE_SCORE", "TEST_INPUT_FILE", "TEST_FILE",
-    #                   "TEST_GENERATED", "TEST_MATCHES_EXPECTED_TEST_FILE",
-    #                   "TEST_USED_CORRECT_FRAMEWORK", "TEST_HAS_ERRORS",
-    #                   "TEST_HAS_TYPESCRIPT_ERRORS", "TEST_LANGUAGE"]]
-
     selected_test = st.selectbox(
         "Select a test", df["TEST_NAME"].unique())
 
@@ -138,13 +135,12 @@ def unit_test_csv_table(name: str):
         st.text("Imported correct framework: " +
                 emojify(row["TEST_USED_EXPECTED_TEST_FRAMEWORK"]))
         if row["TEST_LANGUAGE"] == "typescript":
-            st.text("Is error free? " +
-                    emojify(not row["TEST_HAS_TYPESCRIPT_ERRORS"]))
+            has_errors = bool(row["TEST_HAS_TYPESCRIPT_ERRORS"])
+            st.text("Is error free? " + emojify(not has_errors))
+            if has_errors:
+                st.text("Errors:")
+                st.code(row["TEST_DIAGNOSTICS"], language="text")
         st.code(row["TEST_GENERATED"], language=row['TEST_LANGUAGE'])
-    # pivot_df = df.pivot(index="FILEPATH",
-    #                     columns="FIXTURE", values="LLM_JUDGE_SCORE")
-    # pivot_df = pivot_df.sort_values(by=models_by_score, ascending=False)
-    # pivot_df = pivot_df[models_by_score]
 
 
 chatTab, fixTab, unitTestTab, editTab, autocompleteTab = st.tabs(
